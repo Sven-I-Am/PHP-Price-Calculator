b7030b49cecef16d4b14df72b33a3ac3814bf9ca
RedMarkD <reinout.de.bleser@gmail.com>
2021-11-09 08:46:03 +0100

    project reboot

--- Controller/HomepageController.php
+++ Controller/HomepageController.php
@@ -7,11 +7,7 @@ class HomepageController
     public function render(array $GET, array $POST)
     {
         //this is just example code, you can remove the line below
-        /*insert select input here*/
-//        if ($_POST('select') !== null) {
-//        }
-        $customer = new Customer(" Reinout", "De Bleser");
-
+        $user = new User('John Smith');
 
         //you should not echo anything inside your controller - only assign vars here
         // then the view will actually display them.
--- View/homepage.php
+++ View/homepage.php
@@ -1,56 +1,11 @@
-<?php require 'includes/header.php';
- require 'env.php'?>
-
+<?php require 'includes/header.php'?>
 <!-- this is the view, try to put only simple if's and loops here.
 Anything complex should be calculated in the model -->
 <section>
-    <form>
-        <section>
-            <select name="select">
-                <?php //todo: logica voor dropdown?>
-            </select>
-
-        </section>
-
-    </form>
-
-    <h4>Hello <?php echo $customer->getFullName()?>,</h4>
+    <h4>Hello <?php echo $user->getName()?>,</h4>
 
     <p><a href="index.php?page=info">To info page</a></p>
 
-    <?php
-
-    class TableRows extends RecursiveIteratorIterator {
-        function __construct($it) {
-            parent::__construct($it, self::LEAVES_ONLY);
-        }
-
-        function current() {
-            return "<option>" . parent::current(). "</option>";
-        }
-    }
-
-    $servername = $server;
-    $username = $login;
-    $password = $pass;
-    $dbname = $db;
-
-    try {
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $stmt = $conn->prepare("SELECT firstname, lastname FROM Customer");
-        $stmt->execute();
-
-        // set the resulting array to associative
-        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-        foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
-            echo $v;
-        }
-    } catch(PDOException $e) {
-        echo "Error: " . $e->getMessage();
-    }
-    $conn = null;
-    echo "</table>";
-    ?>
+    <p>Put your content here.</p>
 </section>
 <?php require 'includes/footer.php'?>
